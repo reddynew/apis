c2c30dacf42cbfb6c0f3725a5450d365c6603370
reddynew <hanumanreddyy@gmail.com>
2024-08-12 21:22:38 +0530

    Add files via upload

---
+++ api/basic.py
@@ -0,0 +1,20 @@
+from flask import Flask,jsonify,request
+import pandas as pd
+import requests
+app=Flask(__name__)
+data=pd.read_csv(r'C:\Users\hanum\OneDrive\Desktop\view\apis\cause_list _format.csv')
+api_key='basickey'
+@app.route('/',methods=['GET'])
+def basic():
+    return 'first api using flask',200
+@app.route('/get_data',methods=['GET'])
+def get_data():
+    # if request.headers.get('Authorization') == f'Bearer {api_key}':
+    key = request.args.get('api_key')
+    if key== api_key:
+        return jsonify(data.to_dict(orient='records'))
+    else:
+        return 'No Authorization'
+if __name__ == '__main__':
+    app.run(debug=True)
+        
